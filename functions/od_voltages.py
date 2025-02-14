404aba47ae646a1b3c9cd77a71752d57b6368faf
Michele <michele.cespa@gmail.com>
2025-02-14 16:58:55 +0000

    added optical density voltage readings

--- functions/od_voltages.py
+++ functions/od_voltages.py
@@ -1,13 +1,17 @@
-import adafruit_ads1x15.ads1115 as ADS_1
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 import adafruit_ads7830.ads7830 as ADS_2
+import time
 
-adc1 = ADS.ADS1115(address=0x49, i2c=i2c)
+i2c = busio.I2C(board.SCL,board.SDA)
+adc1 = ADS.ADS1115(address=0x48, i2c=i2c)
 
 channels = [AnalogIn(adc1,ADS.P0),AnalogIn(adc1,ADS.P1),
 AnalogIn(adc1,ADS.P2),AnalogIn(adc1,ADS.P3)]
 
-adc = ADC.ADS7830(i2c)
+adc2 = ADS_2.ADS7830(i2c)
 REF = 4.2
 
 while True: 	
@@ -15,7 +19,7 @@ while True:
 	for ele in channels:
 		v = round(ele.voltage,3)
 		v_s1.append(v)
-	channels2 = [adc.read(i) for i in range(8)]
+	channels2 = [adc2.read(i) for i in range(8)]
 	v_s2 = [round((ch / 65535.0) * REF,3) for ch in channels2]
 	print(v_s1)
 	print(v_s2)
